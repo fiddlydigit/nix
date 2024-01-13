d88f4c719451ed3c01dd5f90e081befe923f949c
sasha <alexandr.baranovschi@gmail.com>
2024-01-13 19:41:50 +0000

    FEAT:

--- programs/nvim.nix
+++ programs/nvim.nix
@@ -2,17 +2,9 @@
 let
 in
 {
-  home.packages = with pkgs; [
-    
-  ]; 
   home.file."./.config/nvim/" = {
     source = ./nvim;
     recursive = true;
-  };
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    coc.enable = false;
   };
  
 }
