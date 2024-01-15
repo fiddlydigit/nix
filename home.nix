9e892edc6298c412d9661a8844db8d2676d6db3d
fiddlydigits <fiddlydigit@gmail.com>
2024-01-15 20:04:00 +0000

    FEAT:

--- home.nix
+++ home.nix
@@ -18,21 +18,12 @@ in
   home.stateVersion = "23.11"; # Please read the comment before changing.
   home.packages = with pkgs; [
     obsidian
-    firefox
-    jetbrains.rider
     lazygit
     ranger
     xclip
     zip
-    lazygit
     neofetch
     unzip
-    google-chrome
-    distrobox
-    nano
-    tmux
-    neovim
-    dotnet-sdk_7
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ]; 
   # Let Home Manager install and manage itself.
