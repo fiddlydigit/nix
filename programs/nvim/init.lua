ef0d050abe232d40e2c44579d4b16ab7c4bd2afa
fiddlydigits <fiddlydigit@gmail.com>
2024-01-21 16:23:46 +0000

    FEAT:

--- programs/nvim/init.lua
+++ programs/nvim/init.lua
@@ -15,17 +15,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
-    'tpope/vim-fugitive',                                 --Git
-    'tpope/vim-sleuth',                                   --Keeps indenation style
-    { 'folke/which-key.nvim',                opts = {} }, --Mappings cheatsheet
-    { 'm4xshen/autoclose.nvim',              opts = {} }, --Brackets and such
-    { 'numToStr/Comment.nvim',               opts = {} }, --Comments
-    { 'lukas-reineke/indent-blankline.nvim', main = 'ibl',                                             opts = {} },
-    { 'neovim/nvim-lspconfig',               dependencies = require('plugins.dependencies.lsp'), },
-    { 'hrsh7th/nvim-cmp',                    dependencies = require('plugins.dependencies.cmp'), },
-    { 'nvim-telescope/telescope.nvim',       dependencies = require('plugins.dependencies.telescope'), branch = '0.1.x', },
-    { 'nvim-treesitter/nvim-treesitter', dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', }, build = ':TSUpdate',
-    },
+    { 'tpope/vim-fugitive', },                                                                                                      -- Git
+    { 'tpope/vim-sleuth', },                                                                                                        -- Keeps indenation style
+    { 'folke/which-key.nvim',                opts = {} },                                                                           -- Mappings cheatsheet
+    { 'numToStr/Comment.nvim',               opts = {} },                                                                           -- Comments
+    { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },                                                             -- Indenation
+    require 'plugins.configs.lspconfig',                                                                                            -- Language servers
+    { 'hrsh7th/nvim-cmp',                dependencies = require('plugins.dependencies.cmp'), },                                     -- Completion
+    { 'nvim-telescope/telescope.nvim',   dependencies = require('plugins.dependencies.telescope'),          branch = '0.1.x', },    -- Search
+    { 'nvim-treesitter/nvim-treesitter', dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', }, build = ':TSUpdate', }, -- Syntax and such
     require 'plugins.configs.gitsigns',
     require 'plugins.configs.theme',
     require 'plugins.configs.lualine',
@@ -38,5 +36,4 @@ require('plugins.configs.telescope')
 require('plugins.configs.treesitter')
 require('plugins.configs.whichkey')
 require('plugins.configs.lsp')
-require('plugins.configs.random')
 require('plugins.configs.cmp')
