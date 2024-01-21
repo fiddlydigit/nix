bdf7e939ae7c99cfc9b31071e87fb54b2c3ad0a4
fiddlydigits <fiddlydigit@gmail.com>
2024-01-21 00:34:19 +0000

    FEAT:

--- programs/nvim/lua/options.lua
+++ programs/nvim/lua/options.lua
@@ -29,3 +29,13 @@ end
 -- Window-local options
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
