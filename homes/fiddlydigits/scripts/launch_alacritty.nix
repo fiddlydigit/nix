fa2e3bad23b34cedfc2a382c30af519fa3fa46d7
fiddlydigits <fiddlydigit@gmail.com>
2024-02-04 01:07:09 +0000

    FEAT: Scripts, just add a new script to folder

--- homes/fiddlydigits/scripts/launch_alacritty.nix
+++ homes/fiddlydigits/scripts/launch_alacritty.nix
@@ -1,7 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 let
-  alacritty = pkgs.writeShellScriptBin "nixGLIntel_alacritty.sh" builtins.readFile ../../scripts/nixGLIntel_alacritty.sh;
+
+  scriptsDir = ../../scripts;
+  isShellScript = name: type: type == "regular" && builtins.match ".*\\.sh$" name != null; # filter
+  shellScripts = (lib.attrsets.filterAttrs isShellScript) (builtins.readDir scriptsDir); # attribute set
+
+  # Create a script that can be run by its name
+  makeScript = scriptName:
+    let
+      scriptPath = scriptsDir + "/${scriptName}";
+      scriptContent = builtins.readFile scriptPath;
+    in
+    pkgs.writeShellScriptBin scriptName scriptContent;
+  
+  # Create a list of scripts
+  scriptPackages = lib.mapAttrsToList # pckgs function
+                   (name: script: makeScript name) # arg1(function) function defined above
+                   shellScripts; # arg2(attribute set) all .sh files in directory
+
 in {
-  home.packages = [ alacritty ];
+  home.packages = scriptPackages;
 }
+
--- homes/fiddlydigits/shortcuts.nix
+++ homes/fiddlydigits/shortcuts.nix
@@ -8,7 +8,7 @@ dconf.settings = {
   };
   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
     binding = "<super>t";
-    command = "/home/${user}/.nix-profile/bin/nixGLIntel_alacritty.sh";
+    command = "nixGLIntel_alacritty.sh";
     name = "open-alacritty";
   };
 };
