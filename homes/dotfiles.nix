f2f520f70861c54fd7e9231b8a811f67714b1b18
fiddlydigits <fiddlydigit@gmail.com>
2024-02-04 18:51:43 +0000

    FEAT: no kitty for you

--- homes/dotfiles.nix
+++ homes/dotfiles.nix
@@ -1,6 +1,5 @@
 {
   ".config/ranger/rc.conf" = import ../packages/dotfiles/ranger-rc.nix;
   ".config/ranger/rifle.conf" = import ../packages/dotfiles/ranger-rifle.nix;
-  ".config/kitty/kitty.conf" = import ../packages/dotfiles/kitty-config.nix;
   ".config/kitty/current-theme.conf" = import ../packages/dotfiles/kitty-theme-noclown.nix;
 }
