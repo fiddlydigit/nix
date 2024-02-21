2d4919cd148d7d690db02f97db59b38455b35c69
fiddlydigits <fiddlydigit@gmail.com>
2024-02-21 12:13:25 +0000

    FEAT: SSh aliases

--- homes/sasha/bash.nix
+++ homes/sasha/bash.nix
@@ -14,6 +14,11 @@
     rebuild = "home-manager switch --flake /home/$(whoami)/Github/nix#$(whoami)";
     mqtt = "/home/$(whoami)/Apps/MQTTExplorer.AppImage";
     coreflux = "/home/$(whoami)/Apps/broker/CorefluxCentral";
+    # ssh
+    do-rita = "ssh -i ~/.ssh/sasha_d_ocean root@165.22.42.237";
+    do-backend = "ssh -i ~/.ssh/sasha_d_ocean root@165.22.46.192";
+    do-frontend = "ssh -i ~/.ssh/sasha_d_ocean root@138.197.105.237";
+    do-downloads = "ssh -i ~/.ssh/sasha_d_ocean root@167.172.243.66";
     # git
     g = "git";
     lg = "lazygit";
