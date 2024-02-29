686eb332b4ff400a515bb3e2b58d379ea2c70e7f
fiddlydigits <fiddlydigit@gmail.com>
2024-02-29 20:25:52 +0000

    TEST: nvim config

---
+++ home-dotfiles-sasha/nvim.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+{
+  home.file."./.config/nvim/" = {
+    source = ./nvim;
+    recursive = true;
+  };
+ 
+}
---
+++ home-dotfiles-sasha/nvim/lua/user/init.lua
@@ -0,0 +1,6 @@
+require("user.options")
+require("user.lazy")
+require("user.keymaps")
+require("user.highlight-yank")
+require("user.format-on-save")
+require("user.keymaps-helper")
