41b1a1e671ba8c992e4fa7f5eeaba24ca87d6fe2
fiddlydigits <fiddlydigit@gmail.com>
2024-03-02 15:59:25 +0000

    FEAT: TODO plugin nvim

--- home-dotfiles-sasha/nvim/lua/plugins/lsp.lua
+++ home-dotfiles-sasha/nvim/lua/plugins/lsp.lua
@@ -7,41 +7,16 @@ return {
     { 'folke/neodev.nvim', }
   },
   config = function()
-    -- [[ Configure LSP ]]
-    -- This function gets run when an LSP connects to a particular buffer.
-    local on_attach = function(_, bufnr)
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = 'LSP: ' .. desc
-        end
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-      end
-
-      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-      nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-      nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-      nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-      nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-      -- Lesser used LSP functionality
-      nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[W]orkspace [L]ist Folders')
+    local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds
+    local on_attach = function(_, buffer_number)
+      map_lsp_keybinds(buffer_number)
 
       -- Create a command `:Format` local to the LSP buffer
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+      vim.api.nvim_buf_create_user_command(buffer_number, 'Format', function(_)
         vim.lsp.buf.format()
       end, { desc = 'Format current buffer with LSP' })
     end
 
-    -- Enable the following language servers
     local servers = {
       rust_analyzer = {},
       nil_ls = {},
--- home-dotfiles-sasha/nvim/lua/user/keymaps.lua
+++ home-dotfiles-sasha/nvim/lua/user/keymaps.lua
@@ -8,24 +8,25 @@ local M = {}
 
 local TERM = os.getenv("TERM")
 
--- Normal --
 -- Disable Space bar since it'll be used as the leader key
 nnoremap("<space>", "<nop>")
+vnoremap("<space>", "<nop>")
 
+----------- Normal ------------------------
+
+-- Press 'U' for redo
+nnoremap("U", "<C-r>")
 -- Save with leader key
 nnoremap("<leader>w", "<cmd>w<cr>", { silent = false })
 -- Quit with leader key
 nnoremap("<leader>q", "<cmd>q<cr>", { silent = false })
 -- Save and Quit with leader key
 nnoremap("<leader>z", "<cmd>wq<cr>", { silent = false })
---- TEST OLD
-vim.keymap.set({ 't' }, '<C-q>', '<C-\\><C-n>')   -- quit Terminal mode, but leave the terminal window open and go to normal mode
-vim.keymap.set('t', '<A-t>', '<C-\\><C-n>:q<CR>') -- quit the terminal window
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
--- Diagnostic keymaps
--- TEST OLD
+
+----------- Navigation (Normal) ------------
+
+nnoremap('k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+nnoremap('j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 -- Center buffer while navigating
 nnoremap("<C-u>", "<C-u>zz")
 nnoremap("<C-d>", "<C-d>zz")
@@ -37,6 +38,7 @@ nnoremap("G", "Gzz")
 nnoremap("gg", "ggzz")
 nnoremap("<C-i>", "<C-i>zz")
 nnoremap("<C-o>", "<C-o>zz")
+-- Find stuff (:noh clears). Need better keymap for this
 nnoremap("%", "%zz")
 nnoremap("*", "*zz")
 nnoremap("#", "#zz")
@@ -52,54 +54,45 @@ end)
 nnoremap("L", "$")
 nnoremap("H", "^")
 
--- Press 'U' for redo
-nnoremap("U", "<C-r>")
+----------- Diagnostics (Normal) ----------
 
+nnoremap('<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
--- Diagnostics
-nnoremap('<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
--- Goto next diagnostic of any severity
-nnoremap("]d", function()
+nnoremap('<leader>dna', function()
   vim.diagnostic.goto_next({})
   vim.api.nvim_feedkeys("zz", "n", false)
-end)
+end, { desc = 'Goto [n]ext diagnostic of [a]ny severity' })
 
--- Goto previous diagnostic of any severity
-nnoremap("[d", function()
+nnoremap('<leader>dpa', function()
   vim.diagnostic.goto_pev({})
   vim.api.nvim_feedkeys("zz", "n", false)
-end)
+end, { desc = 'Goto [p]revious diagnostic of [a]ny severity' })
 
--- Goto next error diagnostic
-nnoremap("]e", function()
+nnoremap("<leader>dn", function()
   vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
   vim.api.nvim_feedkeys("zz", "n", false)
-end)
+end, { desc = 'Goto [n]ext error' })
 
--- Goto previous error diagnostic
-nnoremap("[e", function()
+nnoremap("<leader>dp", function()
   vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
   vim.api.nvim_feedkeys("zz", "n", false)
-end)
+end, { desc = 'Goto [p]revious error' })
 
--- Goto next warning diagnostic
-nnoremap("]w", function()
+nnoremap('<leader>dnw', function()
   vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
   vim.api.nvim_feedkeys("zz", "n", false)
-end)
+end, { desc = 'Goto [n]ext [w]arning' })
 
--- Goto previous warning diagnostic
-nnoremap("[w", function()
+nnoremap('<leader>dpw', function()
   vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
   vim.api.nvim_feedkeys("zz", "n", false)
-end)
+end, { desc = 'Goto [p]revious [w]arning' })
 
--- Open the diagnostic under the cursor in a float window
 nnoremap("<leader>d", function()
   vim.diagnostic.open_float({
     border = "rounded",
   })
-end)
+end, { desc = 'Diagnos under cursor' })
 
 -- Press leader f to format
 nnoremap("<leader>f", ":Format<cr>")
@@ -117,43 +110,48 @@ nnoremap("<leader>gf", function()
     "<(git diff --name-only)",
     "<(git diff --name-only --diff-filter=U)",
   }
-end, { desc = "Search [G]it [F]iles" })
+end, { desc = "Search [g]it [f]iles" })
 
 -- Telescope keybinds --
 local telescope_b = require('telescope.builtin')
-nnoremap("<leader>?", telescope_b.oldfiles, { desc = "[?] Find recently opened files" })
-nnoremap("<leader>sb", telescope_b.buffers, { desc = "[S]earch Open [B]uffers" })
-nnoremap("<leader>sh", telescope_b.help_tags, { desc = "[S]earch [H]elp" })
-nnoremap("<leader>sg", telescope_b.live_grep, { desc = "[S]earch by [G]rep" })
---
+
+nnoremap('<leader>st', '<cmd>TodoTelescope<cr>', { desc = '[?] [s]earch todo notes' })
+nnoremap('<leader>?', telescope_b.oldfiles, { desc = "[?] Find recently opened files" })
+nnoremap('<leader><space>', telescope_b.buffers, { desc = "[s]earch Open [b]uffers" })
+nnoremap('<leader>sh', telescope_b.help_tags, { desc = "[s]earch [h]elp" })
+nnoremap('<leader>sg', telescope_b.live_grep, { desc = "[s]earch by [g]rep" })
+nnoremap('<leader>sw', telescope_b.grep_string, { desc = '[s]earch current [w]ord' })
+nnoremap('<leader>sd', telescope_b.diagnostics, { desc = '[s]earch [d]iagnostics' })
+
 nnoremap("<leader>sf", function()
   telescope_b.find_files({ hidden = true })
-end, { desc = "[S]earch [F]iles" })
---
+end, { desc = "[s]earch [f]iles" })
+
 nnoremap("<leader>sc", function()
   telescope_b.commands(require("telescope.themes").get_dropdown({
     previewer = false,
   }))
-end, { desc = "[S]earch [C]ommands" })
---
+end, { desc = "[s]earch [c]ommands" })
+
 nnoremap("<leader>/", function()
   telescope_b.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-    previewer = false,
+    previewer = true,
+    winblend = 10,
   }))
 end, { desc = "[/] Fuzzily search in current buffer]" })
---
+
 nnoremap("<leader>ss", function()
   telescope_b.spell_suggest(require("telescope.themes").get_dropdown({
     previewer = false,
   }))
-end, { desc = "[S]earch [S]pelling suggestions" })
+end, { desc = "[s]earch [s]pelling suggestions" })
 
 -- LSP Keybinds (exports a function to be used in ../../after/plugin/lsp.lua b/c we need a reference to the current buffer) --
 M.map_lsp_keybinds = function(buffer_number)
-  nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = buffer_number })
-  nnoremap("<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction", buffer = buffer_number })
+  nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [r]e[n]ame", buffer = buffer_number })
+  nnoremap("<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [c]ode [a]ction", buffer = buffer_number })
 
-  nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = buffer_number })
+  nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [g]oto [d]efinition", buffer = buffer_number })
 
   -- Telescope LSP keybinds --
   nnoremap(
@@ -186,8 +184,8 @@ M.map_lsp_keybinds = function(buffer_number)
   inoremap("<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
 
   -- Lesser used LSP functionality
-  nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number })
-  nnoremap("td", vim.lsp.buf.type_definition, { desc = "LSP: [T]ype [D]efinition", buffer = buffer_number })
+  nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [g]oto [d]eclaration", buffer = buffer_number })
+  nnoremap("td", vim.lsp.buf.type_definition, { desc = "LSP: [t]ype [d]efinition", buffer = buffer_number })
 end
 
 
@@ -196,9 +194,6 @@ end
 inoremap("jj", "<esc>")
 
 -- Visual --
--- Disable Space bar since it'll be used as the leader key
-vnoremap("<space>", "<nop>")
-
 -- Press 'H', 'L' to jump to start/end of a line (first/last char)
 vnoremap("L", "$<left>")
 vnoremap("H", "^")
@@ -225,7 +220,10 @@ end)
 -- Enter normal mode while in a terminal
 tnoremap("<esc>", [[<C-\><C-n>]])
 tnoremap("jj", [[<C-\><C-n>]])
-
+-- quit Terminal mode, but leave the terminal window open and go to normal mode
+tnoremap('<C-q>', '<C-\\><C-n>')
+-- quit the terminal window
+tnoremap('<A-t>', '<C-\\><C-n>:q<CR>') -- Better wripe line navigation
 -- Window navigation from terminal
 tnoremap("<C-h>", [[<Cmd>wincmd h<CR>]])
 tnoremap("<C-j>", [[<Cmd>wincmd j<CR>]])
